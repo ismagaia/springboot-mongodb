a9ad4c055f5bc89a19bcb3588532b8cb90909ce3
ismagaia <matheusduartec96@gmail.com>
2020-12-03 09:46:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/maduc/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.maduc.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maduc.workshopmongo.domain.Post;
+import com.maduc.workshopmongo.repository.PostRepository;
+import com.maduc.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+		
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
